6945a33d71c0c5994a16a25a7e7fc5721b16bbdb
usman <usmanganishaik99@gmail.com>
2023-12-15 21:03:28 +0530

    s

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.id
   instance_type = var.instance_type
-  security_groups = var.security_groups
+  security_groups = var.vpc_security_group_ids
 
   tags = {
     Name = var.name
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "instance_type" {}
-variable "security_groups" {}
+variable "vpc_security_group_ids" {}
 variable "zone_id" {}
 variable "name" {}
